9c4727730143b76d78bef97fbc7f97eab79b7749
sandriele27 <sandrielenasc1234@gmail.com>
2025-01-24 14:53:13 +0000

    criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+int main(){
+    printf("Desafio super Trunfo - PaÌses!\n");
+}
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
